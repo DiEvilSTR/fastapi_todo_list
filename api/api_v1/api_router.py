0024e1352183fef5fac511b9e8abf1e993129836
DiEvilSTR <dievilstr@gmail.com>
2023-04-20 23:33:54 +0900

    Add logout

--- api/api_v1/api_router.py
+++ api/api_v1/api_router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from .endpoints import login, tasks, users
+from .endpoints import auth, tasks, users
 
 api_router = APIRouter()
 
-api_router.include_router(login.router, tags=["users"])
+api_router.include_router(auth.router, tags=["users"])
 api_router.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
