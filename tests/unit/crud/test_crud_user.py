e289132f209206a8dd84393dcc5fd8748bb69236
DiEvilSTR <dievilstr@gmail.com>
2023-05-05 12:41:14 +0900

    Fixing update user test

--- tests/unit/crud/test_crud_user.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
-
-from api.api_v1.endpoints.users import read_user
-from core.config import settings
-from crud.crud_user import get_users
-from tests.conftest import get_test_db
-from tests.utils.utils import random_lower_string
